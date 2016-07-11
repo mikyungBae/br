1cb7d93cbd3c5d6bd1358d8b39da0fe323d171b5
mikyungBae <gg5085@naver.com>
2016-07-11 12:18:32 +0900

    Add files via upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  resources :posts do 
+    resources :comments
+  end
 
+    get 'posts/index'
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  
+   root 'posts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
